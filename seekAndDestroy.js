23b91048f45e06713b7ef81997fed8b127368f84
Bondan Sigit <bondansigit@gmail.com>
2022-11-28 23:03:54 +0700

    forth commit

--- seekAndDestroy.js
+++ seekAndDestroy.js
@@ -1,10 +1,17 @@
+// Seek and Destroy
+// You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments. Remove all elements from the initial array that are of the same value as these arguments.
+
+// Note: You have to use the arguments object.
+
 function destroyer() {
+    // separate arguments
     const args = Array.from(arguments)
-    //console.log(args)
     const firstArr = args.shift()
-    // console.log(firstArr)
-  
+    
+    // create new variabel to store array result
     const newArray = []
+
+    // compare if array is not in the arguments, put array in new array
     for (let i = 0; i < firstArr.length; i++) {
       if (args.includes(firstArr[i]) === false ) {
         newArray.push(firstArr[i])
